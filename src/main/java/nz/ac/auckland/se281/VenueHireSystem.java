3fd2b9e0f17745cb42a7cf462dd77c09a9aeef59
Kai Sato <kai.sato321@gmail.com>
2024-04-08 15:34:37 +1200

    Added exceptions in makeBooking if system date is not set or there are no venues

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -89,7 +89,14 @@ public class VenueHireSystem {
   }
 
   public void makeBooking(String[] options) {
-    // TODO implement this method
+    if (this.systemDate == "") {
+      MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
+      return;
+    }
+    if (this.venues.size() == 0) {
+      MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
+      return;
+    }
   }
 
   public void printBookings(String venueCode) {
