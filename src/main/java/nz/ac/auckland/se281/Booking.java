0145a5f8e045d48ce7a83eb3c25a8e34a9ea4b87
Kai Sato <kai.sato321@gmail.com>
2024-04-09 22:05:35 +1200

    Changed indentation 4 -> 2 spaces

--- src/main/java/nz/ac/auckland/se281/Booking.java
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -2,43 +2,43 @@ package nz.ac.auckland.se281;
 
 public class Booking {
 
-    private String bookingReference;
-    private String date;
-    private String email;
-    private String attendees;
+  private String bookingReference;
+  private String date;
+  private String email;
+  private String attendees;
 
-    public Booking(String date, String email, String attendees) {
-        this.bookingReference = BookingReferenceGenerator.generateBookingReference();
-        this.date = date;
-        this.email = email;
-        this.attendees = attendees;
-    }
+  public Booking(String date, String email, String attendees) {
+    this.bookingReference = BookingReferenceGenerator.generateBookingReference();
+    this.date = date;
+    this.email = email;
+    this.attendees = attendees;
+  }
 
-    public String getBookingReference() {
-        return this.bookingReference;
-    }
+  public String getBookingReference() {
+    return this.bookingReference;
+  }
 
-    public String getDate() {
-        return this.date;
-    }
+  public String getDate() {
+    return this.date;
+  }
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+  public void setDate(String date) {
+    this.date = date;
+  }
 
-    public String getEmail() {
-        return this.email;
-    }
+  public String getEmail() {
+    return this.email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getAttendees() {
-        return this.attendees;
-    }
+  public String getAttendees() {
+    return this.attendees;
+  }
 
-    public void setAttendees(String attendees) {
-        this.attendees = attendees;
-    }
+  public void setAttendees(String attendees) {
+    this.attendees = attendees;
+  }
 }
--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -4,58 +4,58 @@ import java.util.ArrayList;
 
 public class Venue {
 
-    private String name;
-    private String code;
-    private int capacity;
-    private int hireFee;
-    private ArrayList<Booking> bookings = new ArrayList<Booking>();
+  private String name;
+  private String code;
+  private int capacity;
+  private int hireFee;
+  private ArrayList<Booking> bookings = new ArrayList<Booking>();
 
-    public Venue(String name, String code, String capacity, String hireFee) {
-        this.name = name;
-        this.code = code;
-        this.capacity = Integer.parseInt(capacity);
-        this.hireFee = Integer.parseInt(hireFee);
-    }
+  public Venue(String name, String code, String capacity, String hireFee) {
+    this.name = name;
+    this.code = code;
+    this.capacity = Integer.parseInt(capacity);
+    this.hireFee = Integer.parseInt(hireFee);
+  }
 
-    public String getName() {
-        return this.name;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getCode() {
-        return this.code;
-    }
+  public String getCode() {
+    return this.code;
+  }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+  public void setCode(String code) {
+    this.code = code;
+  }
 
-    public int getCapacity() {
-        return this.capacity;
-    }
+  public int getCapacity() {
+    return this.capacity;
+  }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
+  public void setCapacity(int capacity) {
+    this.capacity = capacity;
+  }
 
-    public int getHireFee() {
-        return this.hireFee;
-    }
+  public int getHireFee() {
+    return this.hireFee;
+  }
 
-    public void setHireFee(int hireFee) {
-        this.hireFee = hireFee;
-    }
+  public void setHireFee(int hireFee) {
+    this.hireFee = hireFee;
+  }
 
-    public ArrayList<Booking> getBookings() {
-        return this.bookings;
-    }
+  public ArrayList<Booking> getBookings() {
+    return this.bookings;
+  }
 
-    public String addBooking(String date, String email, String attendees) {
-        Booking booking = new Booking(date, email, attendees);
-        this.bookings.add(booking);
-        return booking.getBookingReference();
-    }
+  public String addBooking(String date, String email, String attendees, String systemDate) {
+    Booking booking = new Booking(date, email, attendees);
+    this.bookings.add(booking);
+    return booking.getBookingReference();
+  }
 }
